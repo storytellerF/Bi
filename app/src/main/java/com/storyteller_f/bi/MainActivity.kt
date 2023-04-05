d3a7daf6bad9bf4d3825ea1b632c202be48199bd
storytellerF <34095089+storytellerF@users.noreply.github.com>
2023-04-05 22:03:37 +0800

    show qrcode

--- app/src/main/java/com/storyteller_f/bi/MainActivity.kt
+++ app/src/main/java/com/storyteller_f/bi/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.storyteller_f.bi
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
 import com.storyteller_f.bi.ui.theme.BiTheme
+import java.util.Collections
+import java.util.stream.IntStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +50,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    val image by remember {
+        derivedStateOf {
+            "hello".createQRImage(200, 200)
+        }
+    }
+    val widthDp = LocalConfiguration.current.smallestScreenWidthDp - 100
+
+    Column() {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "request")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "get")
+        }
+        Image(
+            bitmap = image.asImageBitmap(),
+            contentDescription = "test",
+            modifier = Modifier
+                .width(
+                    widthDp.dp
+                )
+                .height(widthDp.dp)
+        )
+    }
+
+}
+
+fun String.createQRImage(width: Int, height: Int): Bitmap {
+    val bitMatrix = QRCodeWriter().encode(
+        this,
+        BarcodeFormat.QR_CODE,
+        width,
+        height,
+        Collections.singletonMap(EncodeHintType.CHARACTER_SET, "utf-8")
+    )
+    return Bitmap.createBitmap(
+        IntStream.range(0, height).flatMap { h: Int ->
+            IntStream.range(0, width).map { w: Int ->
+                if (bitMatrix[w, h]
+                ) Color.BLACK else Color.WHITE
+            }
+        }.toArray(),
+        width, height, Bitmap.Config.ARGB_8888
     )
 }
 
